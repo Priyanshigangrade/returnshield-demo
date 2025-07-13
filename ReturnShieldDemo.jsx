9b6b17a40e9a8e11efb6d14d57d58c1f479d3492
Priyanshi Gangrade <154462517+Priyanshigangrade@users.noreply.github.com>
2025-07-13 13:18:40 +0530

    Update ReturnShieldDemo.jsx

--- ReturnShieldDemo.jsx
+++ ReturnShieldDemo.jsx
@@ -1,14 +1,25 @@
-<div>
-  <label>Upload Seller Images (3 angles):</label><br />
+import { useState } from "react";
 
-  <input type="file" accept="image/*" onChange={(e) => handleSingleSellerImageUpload(e, 0)} /><br />
-  <input type="file" accept="image/*" onChange={(e) => handleSingleSellerImageUpload(e, 1)} /><br />
-  <input type="file" accept="image/*" onChange={(e) => handleSingleSellerImageUpload(e, 2)} />
-</div>
-const handleSingleSellerImageUpload = (e, index) => {
-  const file = e.target.files[0];
-  if (!file) return;
-  const updatedImages = [...sellerImages];
-  updatedImages[index] = file;
-  setSellerImages(updatedImages);
-};
+function ReturnShieldDemo() {
+  const [sellerImages, setSellerImages] = useState([]);
+
+  const handleSingleSellerImageUpload = (e, index) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    const updatedImages = [...sellerImages];
+    updatedImages[index] = file;
+    setSellerImages(updatedImages);
+  };
+
+  return (
+    <div>
+      <label>Upload Seller Images (3 angles):</label><br />
+
+      <input type="file" accept="image/*" onChange={(e) => handleSingleSellerImageUpload(e, 0)} /><br />
+      <input type="file" accept="image/*" onChange={(e) => handleSingleSellerImageUpload(e, 1)} /><br />
+      <input type="file" accept="image/*" onChange={(e) => handleSingleSellerImageUpload(e, 2)} />
+    </div>
+  );
+}
+
+export default ReturnShieldDemo;
